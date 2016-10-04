cf00e9823d168d57317faabeade7868219cdb0eb
ppajda <pajdek@gmail.com>
2016-10-04 22:15:20 +0200

    Update BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -53,11 +53,11 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 
-# Enable dex-preoptimization to speed up first boot sequence (disabled)
-# ifeq ($(HOST_OS),linux)
-# WITH_DEXPREOPT := true
-# DONT_DEXPREOPT_PREBUILTS := true
-# endif
+# Enable dex-preoptimization to speed up first boot sequence
+ifeq ($(HOST_OS),linux)
+WITH_DEXPREOPT := true
+DONT_DEXPREOPT_PREBUILTS := true
+endif
 
 # Keymaster
 TARGET_KEYMASTER_WAIT_FOR_QSEE := true
